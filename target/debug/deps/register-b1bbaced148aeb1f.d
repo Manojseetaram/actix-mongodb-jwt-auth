abd4d6e4fc43d1176c32eaa5b4127b3e53de80c9
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-08-10 21:33:58 +0530

    Ignore target directory

--- target/debug/deps/register-b1bbaced148aeb1f.d
+++
@@ -1,5 +0,0 @@
-/Users/manojseetaramgowda/projects/register/target/debug/deps/register-b1bbaced148aeb1f.d: src/main.rs
-
-/Users/manojseetaramgowda/projects/register/target/debug/deps/libregister-b1bbaced148aeb1f.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/register-b6baf50d17c1a792.d
+++
@@ -1,5 +0,0 @@
-/Users/manojseetaramgowda/projects/register/target/debug/deps/register-b6baf50d17c1a792.d: src/main.rs
-
-/Users/manojseetaramgowda/projects/register/target/debug/deps/libregister-b6baf50d17c1a792.rmeta: src/main.rs
-
-src/main.rs:
