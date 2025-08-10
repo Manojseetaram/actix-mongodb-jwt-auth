85fad178138236fb77534cd2812f3ec594979c65
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-08-10 21:53:47 +0530

    load environment varialbe and also add the env get function

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,10 @@
-use 
+use dotenv::dotenv;
 use std::env;
+
 pub fn load_config(){
-    dotenv.ok()
+    dotenv().ok();
+
+}
+pub fn get_env(key : &str)-> String{
+   env::var(key).unwrap_or_else(|_| panic!(" {:?} must be set in env " , key))
 }
