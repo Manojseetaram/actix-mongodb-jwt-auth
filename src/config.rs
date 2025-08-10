b5f7bb43cd0815d3da6a7cdfbc173fe427dfd15b
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-08-10 21:50:34 +0530

    create file artytechure and also add required dependencies

---
+++ src/config.rs
@@ -0,0 +1,5 @@
+use 
+use std::env;
+pub fn load_config(){
+    dotenv.ok()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod config;
+mod auth;
+mod handles;
+mod models;
+mod db;
+
+
 fn main() {
     println!("Hello, world!");
     println!("This is a manoj")
